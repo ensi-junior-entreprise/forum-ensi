c6f20ef79dfadd3c0264655e6124d29eeb69620f
ensi-junior-entreprise <projet.ensi.junior@gmail.com>
2020-11-27 07:01:41 +0100

    good

--- index.js
+++ index.js
@@ -22,28 +22,39 @@ app.get('/',(req,res)=>{
   res.status(200).send('hello world')
 })
 app.post('/add',upload.single('file'), (req, res) => {
-  let file_test=false
-  if(req.file)
-  {
-    fs.renameSync(req.file.path, req.file.path.replace('undefined', req.body.accord + req.body.nom));
-    file_test=true
-  }
   let data = req.body
-  let user = new User({
-    nom: data.nom,
-    email: data.email,
-    num_tel: data.num_tel,
-    institut: data.institut,
-    accord: data.accord,
-    cv:file_test
-  })
-  user.save()
-  .then(() => {
-    res.status(201).send({ error:false,sent:true })    
-  })
-  .catch((error) => {
-    res.status(400).send({ error:true,sent:false })
-  })
+  User.findOne({email: data.email})
+    .then((mail_test)=>{
+      if(!mail_test){
+        let file_test=false
+        if(req.file)
+        {
+          fs.renameSync(req.file.path, req.file.path.replace('undefined', req.body.accord + req.body.nom));
+          file_test=true
+        }
+        let user = new User({
+          nom: data.nom,
+          email: data.email,
+          num_tel: data.num_tel,
+          institut: data.institut,
+          accord: data.accord,
+          cv:file_test
+        })
+        user.save()
+        .then(() => {
+          res.status(201).send({ error:false,sent:true,msg:false })    
+        })
+        .catch((error) => {
+          res.status(400).send({ error:true,sent:false,msg:false })
+        })
+      }
+      else{
+        res.status(400).send({ error:true,sent:false,msg:true })
+      }
+    }) 
+    .catch((error) => {
+      res.status(400).send({ error:true,sent:false,msg:false })
+    })
 
 })
-app.listen(1998,()=>console.log(process.env.PORT))
+app.listen(1998,()=>console.log("start"))
