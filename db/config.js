395447151263e383662806cd25da429026659730
ensi-junior-entreprise <projet.ensi.junior@gmail.com>
2020-11-27 15:52:27 +0100

    final

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://forum:ensijuni@cluster0.t4jeb.mongodb.net/Forum?retryWrites=true&w=majority"
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://forum:ensijuni2021@cluster0.9qd99.mongodb.net/Forum?retryWrites=true&w=majority"
 
 
 const MONGODB_OPTIONS = {
